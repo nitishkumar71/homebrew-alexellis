94f9ae747974e121dcf12c25517e6d9b29bcd9c6
nitishkumar71 <nitishkumar71@users.noreply.github.com>
2021-01-10 00:47:42 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Portable DevOps Marketplace"
-    url "https://github.com/alexellis/arkade/releases/download/0.6.35/arkade-darwin"
-    sha256 "fa1bf16ab1628cf9398d0d733165cdf35e4ae34446ffded978b31edf5454dcbd"
-    version "0.6.35"
+    url "https://github.com/alexellis/arkade/releases/download/0.6.36/arkade-darwin"
+    sha256 "6885775b4b426060e69057ecb223d86398a98769abd555120723b3e2055d0bdb"
+    version "0.6.36"
     bottle :unneeded
     
     def install
